41cb9706e94f8fe30529f60469eb399e6147ba28
kyletower <ktower@hcps.us>
2019-09-24 10:46:38 -0400

    Update Factory Reset Chrome.bat

--- Factory Reset Chrome.bat
+++ Factory Reset Chrome.bat
@@ -2,4 +2,9 @@
 ECHO Factory Resetting Google Chrome
 RD /S /Q "%LocalAppData%\Google\Chrome"
 ECHO Complete!
+
+ECHO Factory Resetting Edge
+RD /S /Q "%UserProfile%\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
+ECHO Complete!
+
 PAUSE
