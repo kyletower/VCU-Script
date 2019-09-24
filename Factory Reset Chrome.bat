8f8fa44b97c91e19c70f3e0dca26cb0bf568a6af
kyletower <ktower@hcps.us>
2019-09-24 10:47:10 -0400

    Update Factory Reset Chrome.bat

--- Factory Reset Chrome.bat
+++ Factory Reset Chrome.bat
@@ -7,4 +7,8 @@ ECHO Factory Resetting Edge
 RD /S /Q "%UserProfile%\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
 ECHO Complete!
 
+ECHO Emptying Recycle Bins
+RD /S /Q "c:\$Recycle.Bin"
+ECHO Complete!
+
 PAUSE
